ac9b8af32767e55950056929e8d2b3197f5b2344
adevancomp <adevancomp@gmail.com>
2024-06-14 10:36:54 -0400

    adiciona tratamento de CORS

--- api/src/main/kotlin/br/edu/uea/buri/BuriApplication.kt
+++ api/src/main/kotlin/br/edu/uea/buri/BuriApplication.kt
@@ -1,9 +1,12 @@
 package br.edu.uea.buri
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = [Server(url="/", description = "Default Server Url")])
 class BuriApplication
 
 fun main(args: Array<String>) {
