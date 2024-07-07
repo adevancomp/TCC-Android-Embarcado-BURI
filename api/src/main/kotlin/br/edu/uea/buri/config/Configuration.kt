67b2df555651c4b23b07fe1808bfcedb97518f78
adevancomp <adevancomp@gmail.com>
2024-07-07 19:26:22 -0400

    novo 3

--- api/src/main/kotlin/br/edu/uea/buri/config/Configuration.kt
+++ api/src/main/kotlin/br/edu/uea/buri/config/Configuration.kt
@@ -56,7 +56,8 @@ class Configuration: OpenApiCustomizer {
                     "/swagger-resources/**",
                     "/v3/api-docs/**"
                 ).permitAll()
-                auth.requestMatchers(HttpMethod.POST,"/auth","/auth/measurement","/auth/register").permitAll()
+                auth.requestMatchers(HttpMethod.POST,"/auth","/auth/register").permitAll()
+                auth.requestMatchers(HttpMethod.POST,"/auth/measurement").permitAll()
                 auth.anyRequest().authenticated()
         }.sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }.addFilterBefore(authFilter,UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
