4f21c4f408fadea5c3afbaba1da4cc1ee8e832c1
adevancomp <adevancomp@gmail.com>
2024-07-11 13:28:08 -0400

    retira @Transactional Desnecessário

--- api/src/main/kotlin/br/edu/uea/buri/config/security/CustomUserDetailsService.kt
+++ api/src/main/kotlin/br/edu/uea/buri/config/security/CustomUserDetailsService.kt
@@ -1,7 +1,6 @@
 package br.edu.uea.buri.config.security
 
 import br.edu.uea.buri.repository.UserAppRepository
-import jakarta.transaction.Transactional
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
@@ -16,7 +15,6 @@ typealias ApplicationUser = br.edu.uea.buri.domain.UserApp
 class CustomUserDetailsService (
     private val userAppRepository: UserAppRepository
 ) : UserDetailsService {
-    @Transactional
     override fun loadUserByUsername(username: String): UserDetails  {
         val user = userAppRepository.findByEmail(username).getOrNull() ?: throw UsernameNotFoundException("Usuário com name $username não encontrado")
         return User(user.email, user.passwordEncrypted, true, true, true, true,mutableListOf(SimpleGrantedAuthority(user.role.toString())))
