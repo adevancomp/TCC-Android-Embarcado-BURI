39ebf07ce2090624ab59e3457e218c63f267bb1d
adevancomp <adevancomp@gmail.com>
2024-07-25 16:07:10 -0400

    ultima versao

--- api/src/main/kotlin/br/edu/uea/buri/dto/measurement/requests/MeasurementRegisterDTO.kt
+++ api/src/main/kotlin/br/edu/uea/buri/dto/measurement/requests/MeasurementRegisterDTO.kt
@@ -16,7 +16,7 @@ data class MeasurementRegisterDTO (
     val airH: BigDecimal? = null,
     @Digits(integer = 3, fraction = 4)
     val temp: BigDecimal? = null,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
     val collectionDate: ZonedDateTime,
     @NotBlank(message = "equipmentId não pode ser vazio") @Size(min = 6, max = 6, message = "ID deve ter 6 caracteres")
     val equipmentId: String = ""
