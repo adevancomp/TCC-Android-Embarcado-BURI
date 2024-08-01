e9f93b6d96dd319ed387b1ec1cd667a44ab34fbe
adevancomp <adevancomp@gmail.com>
2024-08-01 17:05:48 -0400

    adiciona listener para identificar o clique em um determinado aparelho

--- app/app/src/main/java/br/edu/uea/buri/screens/home/adapter/EquipmentAdapter.kt
+++ app/app/src/main/java/br/edu/uea/buri/screens/home/adapter/EquipmentAdapter.kt
@@ -9,6 +9,7 @@ import br.edu.uea.buri.domain.equipment.Equipment
 class EquipmentAdapter() : RecyclerView.Adapter<EquipmentAdapter.ViewHolder>() {
 
     private var equipments: List<Equipment> = emptyList()
+    var equipmentItemListener: (Equipment)-> Unit = {}
 
     fun submitList(newData: List<Equipment>){
         equipments = newData
@@ -25,10 +26,15 @@ class EquipmentAdapter() : RecyclerView.Adapter<EquipmentAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.equipmentName.text = equipments[position].name
         holder.equipmentId.text = equipments[position].equipmentId
+        holder.content.setOnClickListener {
+            val equipment = equipments[position]
+            equipmentItemListener(equipment)
+        }
     }
 
     inner class ViewHolder(binding: EquipmentItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
+            var content = binding.cvEquipmentItem
             var equipmentName = binding.tvEquipmentName
             var equipmentId = binding.tvEquipmentId
     }
