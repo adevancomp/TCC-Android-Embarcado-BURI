a7358cdfaefa417a9c02c656b53f306e94334d0c
adevancomp <adevancomp@gmail.com>
2024-08-01 17:45:03 -0400

    adiciona tela inicial de informações e cadastro

--- app/app/src/main/java/br/edu/uea/buri/screens/equipment/register/EquipmentRegisterFragment.kt
+++ app/app/src/main/java/br/edu/uea/buri/screens/equipment/register/EquipmentRegisterFragment.kt
@@ -1,7 +1,29 @@
 package br.edu.uea.buri.screens.equipment.register
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import br.edu.uea.buri.databinding.FragmentEquipmentRegisterBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class EquipmentRegisterFragment : Fragment() {
+    private var _binding: FragmentEquipmentRegisterBinding? = null
+    private val binding: FragmentEquipmentRegisterBinding = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEquipmentRegisterBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
