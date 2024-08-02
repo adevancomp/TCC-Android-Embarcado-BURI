c1ac7130fe7c9012e6de96103482d22e20d8bf8e
adevancomp <adevancomp@gmail.com>
2024-08-02 18:38:49 -0400

    solicita permissão em MainActivity

--- app/app/src/main/java/br/edu/uea/buri/screens/MainActivity.kt
+++ app/app/src/main/java/br/edu/uea/buri/screens/MainActivity.kt
@@ -3,15 +3,18 @@ package br.edu.uea.buri.screens
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import br.edu.uea.buri.R
 import br.edu.uea.buri.data.BuriApi
 import br.edu.uea.buri.databinding.ActivityMainBinding
 import br.edu.uea.buri.screens.login.LoginFragment
-import br.edu.uea.buri.screens.login.LoginState
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -23,10 +26,22 @@ class MainActivity : AppCompatActivity() {
     lateinit var shared: SharedPreferences
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val viewModel: MainViewModel by viewModels()
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val allGranted = permissions.values.all { it }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        //Se as permissões não foram concedidas, peça novamente
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissionLauncher.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ))
+        }
         viewModel.isConnected.observe(this){
             isOnline ->
                 if(isOnline){
