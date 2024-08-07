8b3eef76edc1e54b1b56b931e79e1312d5639061
adevancomp <adevancomp@gmail.com>
2024-08-07 17:42:18 -0400

    teste 1

--- api/src/main/kotlin/br/edu/uea/buri/config/Configuration.kt
+++ api/src/main/kotlin/br/edu/uea/buri/config/Configuration.kt
@@ -57,7 +57,7 @@ class Configuration: OpenApiCustomizer {
                     "/v3/api-docs/**"
                 ).permitAll()
             auth.requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
-            //auth.requestMatchers(HttpMethod.GET,"/auth/generateId").permitAll()
+            auth.requestMatchers(HttpMethod.GET,"/auth/generateId").permitAll()
             auth.anyRequest().authenticated()
         }.sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }.addFilterBefore(authFilter,UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
