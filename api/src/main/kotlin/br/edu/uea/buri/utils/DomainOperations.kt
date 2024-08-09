2f4632b036db2fa3e4c5578c684d8b63653c1d6b
adevancomp <adevancomp@gmail.com>
2024-08-09 12:01:30 -0400

    adiciona operations

--- api/src/main/kotlin/br/edu/uea/buri/utils/DomainOperations.kt
+++ api/src/main/kotlin/br/edu/uea/buri/utils/DomainOperations.kt
@@ -1,6 +1,7 @@
 package br.edu.uea.buri.utils
 
 import br.edu.uea.buri.enums.UserRole
+import java.math.BigDecimal
 
 class DomainOperations {
     companion object{
@@ -13,5 +14,25 @@ class DomainOperations {
             UserRole.ROLE_CUSTOMER.toString() -> UserRole.ROLE_CUSTOMER
             else -> UserRole.ROLE_UNDEFINED
         }
+        fun temperatureToMessage(temp: BigDecimal) : String = when {
+           temp > BigDecimal("35.0") -> "Temperatura elevada!"
+           temp < BigDecimal("15.0") -> "Temperatura baixa!"
+           else -> "Temperatura Normal"
+        }
+        fun airHumidityToMessage(air: BigDecimal) : String = when {
+            air > BigDecimal("0.485") && air < BigDecimal("0.623") -> "Umidade do ar adequada"
+            air < BigDecimal("0.352") -> "Estado crítico, baixa umidade do ar"
+            else -> "Estado indefinido"
+        }
+        fun coToMessage(co: BigDecimal) : String = when {
+            co < BigDecimal("35.0") -> "nenhum sintoma adverso dentro de 8 horas de exposição"
+            co >= BigDecimal("35.0") && co < BigDecimal("200.0") -> "dor de cabeça após 2 a 3 horas de exposição"
+            co >= BigDecimal("200.0") && co < BigDecimal("400.0") -> "dor de cabeça e náusea após 1 a 2 horas de exposição"
+            co >= BigDecimal("400.0") && co < BigDecimal("800.0") -> "dor de cabeça, náusea e distúrbios após 45 minutos de exposição; morte em até 2 horas de duração"
+            co >= BigDecimal("800.0") && co < BigDecimal("1000.0") -> "perda de consciência"
+            co >= BigDecimal("1000.0") && co < BigDecimal("1600.0") -> "dor de cabeça, náusea e distúrbios após 5 a 10 minutos de exposição, perda da consciência após 30 minutos de exposição"
+            else -> "efeitos fisiológicos imediatos, perda de consciência e risco de vida após 1 a 3 minutos de exposição"
+        }
+
     }
 }
