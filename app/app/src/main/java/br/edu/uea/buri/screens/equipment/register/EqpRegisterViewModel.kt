f34dc5090dcc9cdee8d0a70d6030e22b3127b2ee
adevancomp <adevancomp@gmail.com>
2024-08-10 14:17:37 -0400

    limpa view model e fragment

--- app/app/src/main/java/br/edu/uea/buri/screens/equipment/register/EqpRegisterViewModel.kt
+++ app/app/src/main/java/br/edu/uea/buri/screens/equipment/register/EqpRegisterViewModel.kt
@@ -1,13 +1,9 @@
 package br.edu.uea.buri.screens.equipment.register
 
 import android.content.SharedPreferences
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import br.edu.uea.buri.data.BuriApi
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,23 +11,5 @@ class EqpRegisterViewModel @Inject constructor(
     private val buriApi: BuriApi,
     private val shared: SharedPreferences
 ) : ViewModel() {
-    private val _id = MutableLiveData<String>("")
-    val id : LiveData<String> = _id
 
-    fun generateId() {
-        viewModelScope.launch {
-            try {
-                val response = buriApi.generateId()
-                if (response.isSuccessful) {
-                    _id.value = response.body()?.id ?: ""
-                } else {
-                    _id.value = "Erro ao gerar ID: ${response.code()}"
-                }
-            } catch (e: Exception) {
-                _id.value = "Exceção ao gerar ID: ${e.message}"
-            }
-        }
-    }
-
-    fun saveEquipment():
 }
--- app/app/src/main/java/br/edu/uea/buri/screens/equipment/register/EquipmentRegisterFragment.kt
+++ app/app/src/main/java/br/edu/uea/buri/screens/equipment/register/EquipmentRegisterFragment.kt
@@ -1,26 +1,16 @@
 package br.edu.uea.buri.screens.equipment.register
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import br.edu.uea.buri.BuildConfig
-import br.edu.uea.buri.R
 import br.edu.uea.buri.databinding.FragmentEquipmentRegisterBinding
 import br.edu.uea.buri.screens.MainViewModel
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,6 +19,10 @@ class EquipmentRegisterFragment : Fragment() {
     private val binding: FragmentEquipmentRegisterBinding get() = _binding!!
     private lateinit var btRegister: MaterialButton
     private lateinit var btOwner: MaterialButton
+    private lateinit var btSaveEquipment: MaterialButton
+    private lateinit var btSaveNewOwner: MaterialButton
+    private lateinit var edtEquipmentName: TextInputEditText
+    private lateinit var edtEquipmentId : TextInputEditText
     private val mainViewModel : MainViewModel by activityViewModels<MainViewModel>()
     private val eqpViewModel: EqpRegisterViewModel by viewModels()
 
@@ -45,41 +39,23 @@ class EquipmentRegisterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setupView()
         setupListeners()
-        eqpViewModel.id.observe(viewLifecycleOwner){ newId ->
-            binding.tvNewIdValue.text = newId
-        }
     }
 
     private fun setupView() {
         btRegister = binding.btNewEquipment
         btOwner = binding.btChangeOwner
+        btSaveEquipment = binding.btSaveEquipment
+        btSaveNewOwner = binding.btSaveNewOwner
+        edtEquipmentName = binding.edtEquipmentName
+        edtEquipmentId = binding.edtEquipmentId
     }
 
     private fun setupListeners() {
         btRegister.setOnClickListener {
-                eqpViewModel.generateId()
-                Snackbar.make(
-                    binding.btNewEquipment,
-                     "Necessário conectar ao wifi Buri para realizar a operação",
-                    Snackbar.LENGTH_LONG)
-                    .setTextColor(
-                        ContextCompat.getColor(requireContext(),R.color.black)
-                    )
-                    .setAction("Copiar URL da API"){
-                        val clipBoard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                        val clip = ClipData.newPlainText("API URL",BuildConfig.BASE_URL_API)
-                        clipBoard.setPrimaryClip(clip)
-                        Toast.makeText(requireContext(),"URL copiada para a área de transferência",Toast.LENGTH_SHORT).show()
-                    }
-                    .setBackgroundTint(
-                        ContextCompat.getColor(requireContext(),R.color.blue_accent)
-                    ).show()
-                binding.tvNewId.isVisible = true
-                binding.tvNewIdValue.isVisible = true
+
         }
         btOwner.setOnClickListener {
-            binding.tvNewId.isVisible = false
-            binding.tvNewIdValue.isVisible =false
+
         }
     }
 
