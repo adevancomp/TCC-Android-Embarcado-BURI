234f3124c3299d6c3862a72137f7375890b9840d
adevancomp <adevancomp@gmail.com>
2024-08-12 18:05:39 -0400

    ainda com bug em deserializer zoned datetime

--- app/app/src/main/java/br/edu/uea/buri/domain/measurement/Measurement.kt
+++ app/app/src/main/java/br/edu/uea/buri/domain/measurement/Measurement.kt
@@ -1,5 +1,6 @@
 package br.edu.uea.buri.domain.measurement
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.math.BigDecimal
@@ -12,6 +13,6 @@ data class Measurement(
     @JsonProperty("carbonMonoxide") val co: BigDecimal?,
     @JsonProperty("airHumidity") val air: BigDecimal?,
     @JsonProperty("temperature") val tmp: BigDecimal?,
-    //@JsonProperty("collectionDate") val collectionDate: LocalDateTime,
+    @JsonProperty("collectionDate") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX") val collectionDate: ZonedDateTime,
     @JsonProperty("equipmentId") val equipmentId: String
 )
